8096e319c26392910dc3db6cad2d3df31fa38784
mossbot1 <43768186+mossbot1@users.noreply.github.com>
2018-10-02 20:51:53 +0100

    helloWorld PHP
    helloWorld PHP

---
+++ helloWorld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>#</title> 
+        <meta name="#" content="#">
+        <meta name="#" content="#">
+    </head>
+
+    <body>
+        <div id = "container">
+        <?php echo '<p>Hello World</p>'; ?> 
+        </div>
+    </body>
+</html>
+
+<!-- mossbot1 20:49 02/10/2018
+HTML Template constructed from https://www.sitepoint.com/a-basic-html5-template/
+PHP example
+-->
